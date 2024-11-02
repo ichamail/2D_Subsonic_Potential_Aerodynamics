a73727e8e262b251bba5d99d474a3fa7fea71c9b
ichamail <107580530+ichamail@users.noreply.github.com>
2024-11-02 22:07:36 +0200

    delete all to refactor code

--- src/myMath/__init__.py
+++
@@ -1,1 +0,0 @@
-from .vector_class import Vector
--- src/utilities/__init__.py
+++
@@ -1,9 +0,0 @@
-from .algorithms import cosspace, DenserAtBoundaries, DenserAtLeadingEdge, DenserAtTrailingEdge
-from .is_inside_polygon import is_inside_polygon
-from .plot_functions import make_legend_arrow
-
-__all__ = (
-    "cosspace", "DenserAtBoundaries", "DenserAtLeadingEdge", "DenserAtTrailingEdge",
-    "is_inside_polygon",
-    "make_legend_arrow"
-)
